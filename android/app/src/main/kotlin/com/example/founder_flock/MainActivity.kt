355d925a7411861448d0ec6814b08d2f6e62a9c9
jamesii-b <jamesbhattarai14@gmail.com>
2024-02-06 09:18:05 +0545

    Initial Commit

---
+++ android/app/src/main/kotlin/com/example/founder_flock/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.founder_flock
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/main.dart
@@ -0,0 +1,22 @@
+
+import 'package:flutter/material.dart';
+import 'package:founder_flock/views/home/home.dart';
+
+
+
+void main() {
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'My App',
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+      ),
+      home: Text(""),
+    );
+  }
+}
